5aacb433e54bec42c0e9b0112b6bcde99cb4b531
JY L <enigsuss@gmail.com>
2023-07-28 00:29:50 +0900

    - added comment to build.gradle: homeServer settings

--- src/main/java/TeamProject/TeamProjectWeb/service/MemberService.java
+++ src/main/java/TeamProject/TeamProjectWeb/service/MemberService.java
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-@Transactional // 조회 시 readOnly = true 해당 속성을 주면 최적화됨
+@Transactional(readOnly = true) // 조회 시 readOnly = true 해당 속성을 주면 최적화됨
 //@AllArgsConstructor // 현재 클래스가 가지고 있는 필드를 가지고 생성자를 만들어줌
 @RequiredArgsConstructor // 현재 클래스가 가지고 있는 필드 중 private final 필드만을 가지고 생성자를 만들어줌
 public class MemberService {
