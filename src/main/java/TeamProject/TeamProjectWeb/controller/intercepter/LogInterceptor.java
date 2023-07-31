d55ec344940e3e9c7fec73fb71743dbcf826f135
didska0524 <juno8779@naver.com>
2023-08-01 04:32:22 +0900

    인터셉터들 추가 커밋, Member (회원가입)과 Timetable 은 사용자 인증과 관련하여 인터셉터가 필요해보임 스프링 시큐리티 사용을 위한 준비

--- src/main/java/TeamProject/TeamProjectWeb/controller/intercepter/LogInterceptor.java
+++ src/main/java/TeamProject/TeamProjectWeb/controller/intercepter/LogInterceptor.java
@@ -7,14 +7,19 @@ import org.springframework.web.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.UUID;
+
+// LogInterceptor는 HTTP 요청과 응답에 대한 로깅을 처리하는 인터셉터
 @Slf4j
 public class LogInterceptor implements HandlerInterceptor {
 
     public static final String LOG_ID = "logId";
 
+    // preHandle 메서드는 요청 전에 호출되며, 요청 정보를 로깅하고 요청을 처리할지 여부를 결정
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
+
+        // UUID 를 생성하여 요청을 구분하는 고유한 로그 ID를 생성하고 요청의 고유 ID를 요청 속성에 저장
 
         String requestURI = request.getRequestURI();
         String uuid = UUID.randomUUID().toString(); //요청 로그를 구분하기 위한 uuid 를 생성
@@ -28,26 +33,34 @@ public class LogInterceptor implements HandlerInterceptor {
             // 일반적으로 @Controller , @RequestMapping 을 활용한 핸들러 매핑을 사용,
             // 핸들러 정보로 HandlerMethod 가 넘어온다
             HandlerMethod hm = (HandlerMethod) handler; //호출할 컨트롤러 메서드의 모든 정보가 포함되어 있다.
+            // hm의 정보를 이용하여 세부적인 로깅을 수행
         }
 
+        // 요청 정보를 로그로 출력
         log.info("REQUEST [{}][{}][{}]", uuid, requestURI, handler);
         return true; //false 진행X
     }
 
+    // postHandle 메서드는 요청 처리 후, 뷰를 렌더링하기 전에 호출
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView)
             throws Exception {
+        // 핸들러 메서드가 실행되고 뷰를 렌더링하는 단계에서 수행할 로직을 작성
         log.info("postHandle [{}]", modelAndView);
     }
 
+    // afterCompletion 메서드는 요청 처리가 완료된 후에 호출되며, 예외 발생 여부와 상관없이 실행
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
             throws Exception {
         String requestURI = request.getRequestURI();
+        // 요청에 저장된 로그 ID를 가져옵니다.
         String logId = (String)request.getAttribute(LOG_ID);
 
+        // 요청 처리가 완료된 후의 로그를 출력
         log.info("RESPONSE [{}][{}]", logId, requestURI);
         if (ex != null) {
+            // 예외가 발생한 경우, 에러 로그를 출력
             log.error("afterCompletion error!!", ex);
         }
         //종료 로그를 postHandle 이 아니라 afterCompletion 에서 실행한 이유는, 예외가 발생한 경우
--- src/main/java/TeamProject/TeamProjectWeb/controller/intercepter/LoginCheckInterceptor.java
+++ src/main/java/TeamProject/TeamProjectWeb/controller/intercepter/LoginCheckInterceptor.java
@@ -8,24 +8,24 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
+// LoginCheckInterceptor 는 인증 체크 기능을 수행하는 인터셉터
 @Slf4j
 public class LoginCheckInterceptor implements HandlerInterceptor {
 
+    // preHandle 메서드는 요청 전에 호출되며, 인증 여부를 체크하여 인증되지 않은 사용자의 요청을 처리합니다.
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
-
         String requestURI = request.getRequestURI();
 
-        log.info("인증 체크 인터셉터 실행 {}", requestURI);
+        // 세션에서 로그인 정보를 확인하여 인증되지 않은 사용자인 경우 로그인 페이지로 리다이렉트합니다.
         HttpSession session = request.getSession(false);
-
         if (session == null || session.getAttribute(SessionConst.LOGIN_MEMBER) == null) {
             log.info("미인증 사용자 요청");
-            //로그인으로 redirect
+            // 로그인 페이지로 리다이렉트하며, 로그인 후 다시 이전 페이지로 돌아올 수 있도록 redirectURL 을 지정합니다.
             response.sendRedirect("/login?redirectURL=" + requestURI);
-            return false;
+            return false; // 인증되지 않은 사용자의 요청을 처리하지 않고 종료합니다.
         }
-        return true;
+        return true; // 인증된 사용자의 요청은 다음 단계로 진행하기 위해 true 를 반환합니다.
     }
 }
