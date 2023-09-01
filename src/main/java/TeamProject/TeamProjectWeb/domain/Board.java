22b35f98f33ebd6b856234b94b43c1edf7f5a201
enigsuss <enigsuss@gmail.com>
2023-09-01 17:54:54 +0900

    like_cnt 추가, mysql-board테이블 명칭 통일

--- src/main/java/TeamProject/TeamProjectWeb/domain/Board.java
+++ src/main/java/TeamProject/TeamProjectWeb/domain/Board.java
@@ -14,11 +14,11 @@ import static jakarta.persistence.CascadeType.ALL;
 
 @Entity
 @Getter @Setter
-@Table(name = "border")
+@Table(name = "board")
 public class Board { // 게시판 클래스
     @Id
     @GeneratedValue // 자동 생성 => 시퀀스
-    @Column(name = "border_id")
+    @Column(name = "board_id")
     private Long id;
     private String title; // 제목
     private String content; // 본문
@@ -32,6 +32,10 @@ public class Board { // 게시판 클래스
     @Enumerated(EnumType.STRING) // DB에 저장할때, enum 각각 요소의 순서(상수)가 아닌, 문자열로 저장
     private BoardKind boardKind; // 게시판 종류
     private String author; // 익명 또는 사용자명을 저장, 게시판에 출력할때 가져오기위함
+    @Column(name = "like_count")
+    private int likeCnt; // 좋아요 개수
+
+
     @OneToMany(mappedBy = "board", cascade = ALL, orphanRemoval = true) // mappedBy : 연관관계 주인이 누구인지 상태 테이블 속성이름으로 명시해줌
     //@JsonManagedReference  // Board엔티티를 직렬화할 때 연관된 엔티티 클래스의 정보는 직렬화하지 않도록 하여 순환 참조로 인한 무한루프 방지
     @JsonIgnore
