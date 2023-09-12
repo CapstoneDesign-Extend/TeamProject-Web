9435559b7323417f39c0da88ea7eaf4e8da6d8e8
didska0524 <juno8779@naver.com>
2023-09-12 10:57:30 +0900

    최적화 밑 동적화 작업 1차

--- src/main/java/TeamProject/TeamProjectWeb/WebConfig.java
+++ src/main/java/TeamProject/TeamProjectWeb/WebConfig.java
@@ -8,8 +8,11 @@ import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -50,4 +53,10 @@ public class WebConfig implements WebMvcConfigurer {
                         "/css/**", "/*.ico", "/error"
                 );
     }
+
+    /*@Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver>
+                                             resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }*/
 }
