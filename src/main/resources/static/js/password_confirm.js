1bbef996b260cd9b16e02570a77412100ed234e7
RazerPointer <jinho0115556@gmail.com>
2023-09-18 21:21:34 +0900

    회원가입 정보 입력창 js수정본 & html수정본

--- src/main/resources/static/js/password_confirm.js
+++ src/main/resources/static/js/password_confirm.js
@@ -1,13 +1,27 @@
 var password = document.getElementById('pwd');
 var password_confirm = document.getElementById('pwd_re');
+var registerbtn = document.getElementsByClassName('register')[0];
+var form = document.getElementById('input_form');
+registerbtn.disabled = true;
+var inputdiv = []
 
-password_confirm.addEventListener('keyup', function(){
+form.addEventListener('keyup', function(){
     if(password_confirm.value != password.value){
         var warn = document.getElementById('not');
         warn.style.display = 'block'
+        registerbtn.disabled = true;
     }
     else if(password_confirm.value == password.value){
         var warn = document.getElementById('not');
         warn.style.display = 'none'
+        for(var i=0; i<5; i++){
+            inputdiv[i] = document.getElementsByClassName('input')[i];
+            console.log(inputdiv[i].value);
+        }
+    
+        if(inputdiv[0].value!=null&& inputdiv[1].value!=null && inputdiv[2].value!=null && inputdiv[3].value!=null && inputdiv[4].value!=null) {
+            registerbtn.disabled = false;
+        }
     }
-})
+});
+
