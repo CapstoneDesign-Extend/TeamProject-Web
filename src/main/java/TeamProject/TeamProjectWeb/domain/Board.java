60bd4454b1262059f685d39050f8ffd60da35da2
enigsuss <enigsuss@gmail.com>
2023-09-28 11:09:22 +0900

    - 해당 오류는 Hibernate가 데이터베이스로부터 가져온 데이터를 엔터티 객체에 매핑할 때 발생하는 문제와 관련이 있습니다. 구체적으로, `Board` 엔터티의 `price` 프로퍼티에 `null` 값을 할당하려고 시도했는데, 이 프로퍼티가 기본 데이터 타입 (primitive type)인 경우 이러한 작업은 허용되지 않기 때문에 오류가 발생합니다.
      기본 데이터 타입은 `null` 값을 허용하지 않습니다. 예를 들어, `int`와 같은 기본 데이터 타입에는 `null` 값을 할당할 수 없습니다. 만약 이 필드가 `null` 값을 허용해야 한다면 해당 필드의 데이터 타입을 해당 타입의 래퍼 클래스로 변경해야 합니다. 예를 들어, `int`의 경우 `Integer`로 변경하면 됩니다.
    
      따라서, 해결 방법은 다음과 같습니다:
    
      1. `Board` 엔터티의 `price` 프로퍼티의 데이터 타입을 해당 타입의 래퍼 클래스로 변경합니다. 예를 들어, `int`를 `Integer`로 변경합니다.
      2. `price` 프로퍼티에 대한 getter와 setter도 해당 래퍼 클래스 타입을 사용하도록 수정합니다.
      3. 필요하다면, 데이터베이스 스키마도 해당 필드가 `null` 값을 허용하도록 수정합니다.
    
      이러한 변경을 통해 Hibernate가 `null` 값을 해당 프로퍼티에 할당할 수 있게 되어 오류가 발생하지 않게 됩니다.

--- src/main/java/TeamProject/TeamProjectWeb/domain/Board.java
+++ src/main/java/TeamProject/TeamProjectWeb/domain/Board.java
@@ -38,7 +38,7 @@ public class Board { // 게시판 클래스
     private int likeCnt; // 좋아요 개수
     @Column(name = "chat_count")
     private int chatCnt;  // 댓글수
-    private int price;
+    private Integer price;  // null을 허용하기 위해 Wrapper클래스 사용
     @JsonIgnore
     @OneToMany(mappedBy = "board", cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE }, orphanRemoval = true) // mappedBy : 연관관계 주인이 누구인지 상태 테이블 속성이름으로 명시해줌
     //== 게시글을 삭제하면 달려있는 댓글 모두 삭제 ==//
