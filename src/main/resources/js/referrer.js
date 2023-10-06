d818419547fae9a41c58ac67dd6c173b6868c223
RazerPointer <jinho0115556@gmail.com>
2023-10-06 17:11:12 +0900

    수정,삭제버튼 추가 및 글작성 페이지 진입시 게시판 인식기능

---
+++ src/main/resources/js/referrer.js
@@ -0,0 +1,17 @@
+var ref = document.referrer;
+var selectbox = document.getElementById('select_value');
+
+window.addEventListener('load',function(){
+    var split_ref = ref.split('/');
+    //alert(split_ref.indexOf('free_board.html?_ijt=rmd42v1atrfubbe8ki4ri5d3ss&_ij_reload=RELOAD_ON_SAVE'));
+    //alert(split_ref);
+    //alert(ref);
+
+    if(split_ref.indexOf('FREE')>=0){
+        selectbox.options[0].selected = true;
+    } else if(split_ref.indexOf('MARKET')>=0) {
+        selectbox.options[1].selected = true;
+    } else if(split_ref.indexOf('QNA')>=0) {
+        selectbox.options[2].selected = true;
+    }
+})
