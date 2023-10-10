ee1f7053098afe44b95672317b39b1716cc405a5
enigsuss <enigsuss@gmail.com>
2023-10-11 07:39:43 +0900

    - 학과특화게시판 구현을 위한 엔드포인트 추가

--- src/main/java/TeamProject/TeamProjectWeb/domain/BoardKind.java
+++ src/main/java/TeamProject/TeamProjectWeb/domain/BoardKind.java
@@ -6,10 +6,10 @@ public enum BoardKind {
     TIP,        // 학과꿀팁
     REPORT,     // 신문고
     QNA,        // QnA
-    MARKET,     // 장터게시판
 
 
     // 학교 단위 게시판 :
+    MARKET,     // 장터게시판
     FREE,       // 자유게시판
     FRESH,      // 새내기게시판
     FOSSIL,     // 졸업생게시판
