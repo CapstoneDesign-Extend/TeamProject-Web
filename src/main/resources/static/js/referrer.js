4209bdaf79121772d91b54be96c53c10d75e93f2
RazerPointer <jinho0115556@gmail.com>
2023-10-11 22:37:08 +0900

    옵션값 & 매핑수정

--- src/main/resources/static/js/referrer.js
+++ src/main/resources/static/js/referrer.js
@@ -7,14 +7,32 @@ window.addEventListener('load',function(){
     //alert(split_ref);
     //alert(ref);
 
-    if(split_ref.indexOf('FREE')>=0){
+    if(split_ref.indexOf('TIP')>=0){
         selectbox.options[0].selected = true;
-        inputbox.value = "FREE";
-    } else if(split_ref.indexOf('MARKET')>=0) {
+        inputbox.value = "TIP";
+    } else if(split_ref.indexOf('REPORT')>=0) {
         selectbox.options[1].selected = true;
-        inputbox.value = "MARKET";
+        inputbox.value = "REPORT";
     } else if(split_ref.indexOf('QNA')>=0) {
         selectbox.options[2].selected = true;
         inputbox.value = "QNA";
+    } else if(split_ref.indexOf('MARKET')>=0) {
+        selectbox.options[3].selected = true;
+        inputbox.value = "MARKET";
+    } else if(split_ref.indexOf('FREE')>=0) {
+        selectbox.options[4].selected = true;
+        inputbox.value = "FREE";
+    } else if(split_ref.indexOf('FRESH')>=0) {
+        selectbox.options[5].selected = true;
+        inputbox.value = "FRESH";
+    } else if(split_ref.indexOf('FOSSIL')>=0) {
+        selectbox.options[6].selected = true;
+        inputbox.value = "FOSSIL";
+    } else if(split_ref.indexOf('INFO')>=0) {
+        selectbox.options[7].selected = true;
+        inputbox.value = "INFO";
+    } else if(split_ref.indexOf('CAREER')>=0) {
+        selectbox.options[8].selected = true;
+        inputbox.value = "CAREER";
     }
 })
